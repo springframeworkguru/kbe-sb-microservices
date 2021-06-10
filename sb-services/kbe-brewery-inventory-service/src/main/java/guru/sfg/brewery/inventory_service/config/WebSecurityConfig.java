7dd736e73b3c307105a5017b35f1ef61a43534ad
John Thompson <john@springframework.guru>
2021-06-10 14:23:04 -0400

    adding sec config for health endpoints

--- sb-services/kbe-brewery-inventory-service/src/main/java/guru/sfg/brewery/inventory_service/config/WebSecurityConfig.java
+++ sb-services/kbe-brewery-inventory-service/src/main/java/guru/sfg/brewery/inventory_service/config/WebSecurityConfig.java
@@ -13,7 +13,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.csrf().disable()
-                .authorizeRequests()
+                .authorizeRequests().mvcMatchers("/actuator/health/**").permitAll()
                 .anyRequest().authenticated()
                 .and()
                 .httpBasic();
